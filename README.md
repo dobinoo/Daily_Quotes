c789695c84c203cb16222ae3d0e7a7880c975133
dobinoo <dobinoo5@gmail.com>
2018-01-23 23:55:03 +0100

    Modified some things in README.md in quote.py added header(hdr) and finally quote WORKINGgit status :)

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ This is a simple python/shell script to gather a daily quote from ```brainyquote
 ### Requirements:
 
 - only python (should work for any version of python, but if it doesn't work try download latest version)
+- you need (obviously) internet connection
 - None other things required
 - None installation required
 - JUST "python quote.py" :)
+- if you still get some errors try to check if website isn't down ```ping brainyquote.com```
 
 ### INFO
 This work is only for educational purpose and mainly to improve my skills in python and try to make and end my projects :). If have any questions or comments, you can send email to ```dobinoo5@gmail.com```.
--- quote.py
+++ quote.py
@@ -1,20 +1,21 @@
 from termcolor import colored   #add colored output here
 import time #to use sleep function for delay
 import urllib2 #for handling content from website
+#import re #imported re but later decided to use built in funcions
 #for using system functions
 #import sys #later removed
 
 #ifdef unfinished = True , then program is not fully finished and he might not be runnable or not propertly
 #ifdef runnable = False , program cannot even run , because of missing part of code, some files etc..
-unfinished = True
+unfinished = False
 runnable = True
 
 #Exit function :)
 def bye():
-    print ("\nExiting . . .")
+    print ("\n\nExiting . . .")
     time.sleep(3)
 
-    print colored("\n\nUse this wisely young Padawan", "yellow", attrs=['bold', 'dark'])
+    print colored("Use this wisely young Padawan", "yellow", attrs=['bold', 'dark'])
 
     exit(0)
     return None
@@ -31,13 +32,45 @@ def check():
 ##########################################
 #main part of code
 
+#calling checking function
 check()
 
+##############
+#manipulating with website
 url = "https://www.brainyquote.com/topics/daily"
-req = urllib2.Request(url)
-response = urllib2.urlopen(req)
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+
+req = urllib2.Request(url, headers=hdr)
+try:
+    response = urllib2.urlopen(req)
+except urllib2.HTTPError, e:
+    print e.fp.read()
 html = response.read()
-print (html[0:300])
+start = html.find('&quot;') + 6
+next = html.find('&quot;',start)
+end = html.find('">',next + 6)
+quote = html[start:next]
+author = html[next + 6:end]
+#manipulating with website
+#######################
+
+########
+#printing part
+print "\nTHE QUOTE OF THE DAY:\n"
+time.sleep(2)
+print colored(quote,"blue", attrs=['bold'])
+time.sleep(2)
+print colored(author,"red", attrs=['bold'])
+#print html
+#printing part
+###########
+time.sleep(3)
+bye()
 
 #end of main part of code
 ##########################################
